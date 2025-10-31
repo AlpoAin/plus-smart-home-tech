4e8512e5310d28c0b09a362407e0acccc68b825c
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-10-31 18:00:03 +0500

    code style

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/api/model/hub/ScenarioRemovedEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/telemetry/collector/api/model/hub/ScenarioRemovedEvent.java
@@ -1,6 +1,16 @@
 package ru.yandex.practicum.kafka.telemetry.collector.api.model.hub;
-import lombok.*; @Getter @Setter @ToString(callSuper = true)
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
 public class ScenarioRemovedEvent extends HubEvent {
+
     private String name;
-    @Override public HubEventType getType(){ return HubEventType.SCENARIO_REMOVED; }
+
+    @Override
+    public HubEventType getType(){
+        return HubEventType.SCENARIO_REMOVED;
+    }
 }
