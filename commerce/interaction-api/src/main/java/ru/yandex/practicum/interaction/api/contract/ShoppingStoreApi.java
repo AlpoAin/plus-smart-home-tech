a126e57e2c88438d6140ebe9290c7b02ba82611c
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-12-21 00:05:28 +0600

    fix: JacksonPageConfig, PageImplJsonSerializer

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/contract/ShoppingStoreApi.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/contract/ShoppingStoreApi.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.interaction.api.dto.store.*;
+import ru.yandex.practicum.interaction.api.dto.store.ProductsPageResponse;
 
 import java.util.List;
 import java.util.UUID;
