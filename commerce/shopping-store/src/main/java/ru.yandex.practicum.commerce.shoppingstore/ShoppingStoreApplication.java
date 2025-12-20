91d1be2a9c9dcf83baed73b8b7783cc073eeffd7
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-12-21 00:05:31 +0600

    fix: port

--- commerce/shopping-store/src/main/java/ru.yandex.practicum.commerce.shoppingstore/ShoppingStoreApplication.java
+++ commerce/shopping-store/src/main/java/ru.yandex.practicum.commerce.shoppingstore/ShoppingStoreApplication.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.commerce.shoppingstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ShoppingStoreApplication {
     public static void main(String[] args) {
