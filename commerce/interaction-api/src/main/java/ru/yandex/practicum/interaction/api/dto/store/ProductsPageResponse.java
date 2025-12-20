05e95c8c7b9a7314b9ab23d82b6f1ea32d04172a
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-12-21 00:05:30 +0600

    fix: ProductsPageResponse

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/dto/store/ProductsPageResponse.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/interaction/api/dto/store/ProductsPageResponse.java
@@ -4,24 +4,20 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record ProductsPageResponse(
-        List<ProductDto> content,
+        @JsonProperty("products") List<ProductDto> products,
         Page page
 ) {
-    @JsonProperty("products")
-    public List<ProductDto> products() { return content; }
-
-    // если нужно сохранить старое имя тоже:
     @JsonProperty("items")
     public List<ProductDto> items() {
-        return content;
+        return products;
     }
 
-    public ProductsPageResponse(List<ProductDto> content,
+    public ProductsPageResponse(List<ProductDto> products,
                                 int number,
                                 int size,
                                 long totalElements,
                                 int totalPages) {
-        this(content, new Page(size, number, totalElements, totalPages));
+        this(products, new Page(size, number, totalElements, totalPages));
     }
 
     public record Page(int size, int number, long totalElements, int totalPages) { }
