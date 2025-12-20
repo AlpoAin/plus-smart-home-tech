899fa2348fc7affa17771f9788fe0d144da7eab0
AlpoAin <a.sabyrgaliyev@yandex.kz>
2025-12-21 00:05:32 +0600

    fix: ProductService

--- commerce/shopping-store/src/main/resources/schema.sql
+++ commerce/shopping-store/src/main/resources/schema.sql
@@ -10,10 +10,3 @@ CREATE TABLE IF NOT EXISTS shopping_store.products (
   product_category VARCHAR NOT NULL,
   price NUMERIC(12,2) NOT NULL
 );
-
-INSERT INTO shopping_store.products
-(product_id, product_name, description, image_src, quantity_state, product_state, product_category, price)
-VALUES
-('00000000-0000-0000-0000-000000000001', 'Alpha', 'Test product Alpha', null, 'ENOUGH', 'ACTIVE', 'CONTROL', 1000.00),
-('00000000-0000-0000-0000-000000000002', 'Beta',  'Test product Beta',  null, 'ENOUGH', 'ACTIVE', 'CONTROL', 900.00),
-('00000000-0000-0000-0000-000000000003', 'Gamma', 'Test product Gamma', null, 'ENOUGH', 'ACTIVE', 'CONTROL', 1100.00);
